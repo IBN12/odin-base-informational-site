b993e988d5a1c610013df205dde3ef7e353346f1
IBN12 <IBN12@github.com>
2024-12-01 20:38:19 -0800

    Reformatted using Express.js. The index, contact, about, and 404 page are being rendered with separate get requests.

--- README.md
+++ README.md
@@ -4,4 +4,7 @@
 This project will contain a node website. It will have a project directory that contains 
 4 pages: index, about, contact-me and 404. These pages won't contain any real information. 
 
+The project has reformatted using Express.js. There are still four pages being rendered 
+with a seperate get request. 
+
 Live Demo: [Basic Informational Site](https://replit.com/@ibenge/odin-base-informational-site)
--- index.js
+++ index.js
@@ -2,81 +2,30 @@ const http = require('node:http');
 const path = require('node:path');
 const fs = require('node:fs'); 
 
-// Will first look for the environment variable, and
-// if thats not found then it will run it on port 8080:
-const PORT = process.env.PORT || 8080;
+const express = require('express');
+const index = express(); 
 
-const server = http.createServer((req, res) => {
-    // Building a file path: 
-    let filePath = "";
-    if (req.url === "/")
-    {
-        filePath = path.join(__dirname, "project", "index.html"); 
-    }
-    else if (req.url === "/about")
-    {
-        filePath = path.join(__dirname, "project", "about.html");
-    }
-    else if (req.url === "/contact-me")
-    {
-        filePath = path.join(__dirname, "project", "contact-me.html"); 
-    }
+index.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'project', 'index.html')); 
+}); 
 
-    // Extension name:
-    let extName = path.extname(filePath); 
+index.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'project', 'about.html')); 
+}); 
 
-    // Initial content type:
-    let contentType = 'text/html';
-
-    // Check the extension name and set the content type:
-    switch(extName)
-    {
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;
-        case '.jpg':
-            contentType = 'image/jpg';
-            break;
-        case '.text':
-            contentType = 'text/plain'; 
-            break;
-    }
-
-    fs.readFile(filePath, (err, content) => {
-        if (err)
-        {
-            if (err.code === 'ENOENT')
-            {
-                // Page not found:
-                fs.readFile(path.join(__dirname, 'project', '404.html'), (err, content) =>{
-                    res.writeHead(200, {'Content-Type': 'text/html'});
-                    res.end(content, 'utf8'); 
-                });
-            }
-            else
-            {
-                // Server error: 
-                res.writeHead(500); 
-                res.end(`Server Error ${err.code}`); 
-            }
-        }
-        else 
-        {
-            
-            // Success:
-            res.writeHead(200, {'Content-Type': contentType});
-            res.end(content, 'utf8');
-        }
-    });
+index.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'project', 'contact-me.html')); 
 });
 
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+index.get('/:id', (req, res) => {
+    if (req.params.id !== '/' || req.params.id !== 'about' || req.params.id !== 'contact')
+    {
+        res.sendFile(path.join(__dirname, 'project', '404.html'));
+    }
+}); 
+
+const PORT = process.env.PORT || 3000;
+index.listen(PORT, () => {
+    console.log(`Testing out express with this project on PORT ${PORT}`); 
+});
+
